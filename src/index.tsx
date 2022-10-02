c747f934baf1c77507a41c746f9dbadd8e1b2d9e
진현성 <gostroad37@gmail.com>
2022-10-03 04:19:58 +0900

    코드 정리

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,14 @@ import "./assets/css/reset.scss"
 import "./assets/css/app.scss"
 import React from "react";
 
-const container = document.getElementById("root") as HTMLElement
-const root = createRoot(container)
-root.render(<React.StrictMode><App /></React.StrictMode >)
 
-// ReactDOM.render(
-//     <App />,
-//     document.getElementById("root")
-// );
+createRoot(
+    document.getElementById("root") as HTMLElement
+).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode >
+)
+
+
 
