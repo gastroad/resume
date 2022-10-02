e335b0d37716bd54bd003f27e9ab8f0d0864978e
진현성 <gostroad37@gmail.com>
2022-10-03 04:17:45 +0900

    react 18 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "core-js";
 import "regenerator-runtime/runtime";
 
 import App from "./App"
 import "./assets/css/reset.scss"
 import "./assets/css/app.scss"
+import React from "react";
 
-ReactDOM.render(
-    <App />,
-    document.getElementById("root")
-);
+const container = document.getElementById("root") as HTMLElement
+const root = createRoot(container)
+root.render(<React.StrictMode><App /></React.StrictMode >)
+
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById("root")
+// );
+
