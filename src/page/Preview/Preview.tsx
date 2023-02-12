b5b0fff7978d5e1c86cf76f4c7625bde551a778d
진현성 <gostroad37@gmail.com>
2023-02-13 07:06:28 +0900

    preview 애니메이션 추가 및 router 연결

--- src/page/Preview/Preview.tsx
+++ src/page/Preview/Preview.tsx
@@ -1,8 +1,22 @@
+import React, { ReactEventHandler, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import cx from "classnames"
+
 const preview = () => {
+
+    const [isActive, setIsActive] = useState(false);
+    const navigate = useNavigate()
+
+    const handleCardClick = (e: React.MouseEvent<HTMLElement>) => {
+        setIsActive(!isActive)
+        setTimeout(() => {
+            navigate("/resume");
+        }, 1000)
+    }
     return (
         <>
-            <div className="preview-wrapper">
-                <div className="card">
+            <div className="preview-wrapper" >
+                <div className={cx("card", { "fade-out": isActive })} onClick={handleCardClick} >
                     <img className="card-img" src={require("../../assets/image/profile.jpeg").default} />
                     <div className="card-txt">
                         <h5 className="title">진현성</h5>
