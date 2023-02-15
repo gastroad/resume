45938f22fde57279166123f9160a6bd351ea3144
진현성 <gostroad37@gmail.com>
2023-02-16 04:44:21 +0900

    information 구조 변경

--- src/components/Information/Information.tsx
+++ src/components/Information/Information.tsx
@@ -2,26 +2,16 @@ import { FC } from "react"
 
 const Information: FC = () => {
     return (
-        <article style={{
-            backgroundColor: "red"
-        }}>
-            <h2 className="h2">
-                인적사항
-            </h2>
-            <div className="card">
-                <div style={{ display: "inline-block", marginRight: "5px" }}>
-                    <p>이름 </p>
-                    <p>생년월일 </p>
-                    <p>email </p>
+        <section>
+            <article className="infomation-wrapper">
+                <h2 className="h2">인적사항</h2>
+                <div>
+                    <p>이름 : <span>진현성</span></p>
+                    <p>생년월일 : <span>2000.02.15</span> </p>
+                    <p>email : <span>coldpotatosweet@gmail.com</span></p>
                 </div>
-                <div style={{ display: "inline-block" }}>
-                    <p>: 진현성</p>
-                    <p>: 2000.02.15</p>
-                    <p>: coldpotatosweet@gmail.com</p>
-                </div>
-
-            </div>
-        </article>
+            </article>
+        </section>
     )
 }
 
